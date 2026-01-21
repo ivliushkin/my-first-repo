b24f2687c073de882d6db177d8e815d295366f53
Denis Ivliushkin <227394763+ivliushkin@users.noreply.github.com>
2026-01-22 00:30:07 +0100

    updated 4 task

--- cyber-assessment/3-header.py
+++ cyber-assessment/3-header.py
@@ -3,4 +3,4 @@ import sys,
 import urllib.request
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+    print(response.headers.get('X-Request-Id', ''))
