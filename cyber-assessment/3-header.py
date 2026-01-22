fe3602c5049d19cd1e1e5940baff1c18f61fae5d
Denis Ivliushkin <227394763+ivliushkin@users.noreply.github.com>
2026-01-22 01:11:10 +0100

    updated 4 task

--- cyber-assessment/3-header.py
+++ cyber-assessment/3-header.py
@@ -3,7 +3,6 @@ import sys
 import urllib.request
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-    xReqId = response.getheader('X-Request-Id')
-    if xReqId is None:
-        xReqId = ''
-    print(xReqId, end='')
+    x_req_id = response.getheader('X-Request-Id')
+    if x_req_id is not None:
+        print(x_req_id)
