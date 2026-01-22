ac89a2901c24d7ebcb30749fa668cf299816a919
Denis Ivliushkin <227394763+ivliushkin@users.noreply.github.com>
2026-01-22 01:12:31 +0100

    updated 6 task

--- cyber-assessment/5-my_github.py
+++ cyber-assessment/5-my_github.py
@@ -1,4 +1,6 @@
-import requests, sys
+#!/usr/bin/python3
+import requests
+import sys
 
 response = requests.get("https://api.github.com/user", auth=(sys.argv[1], sys.argv[2]))
 print(response.json().get("id"))
